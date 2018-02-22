8332b288417d664c01a522c8b940c40554d924c2
sorebear <sorenbaird@gmail.com>
2018-02-21 20:59:09 -0800

    First commit on new branch

--- src/pages/services.js
+++ src/pages/services.js
@@ -15,22 +15,17 @@ export default () => (
         <div style={styles.serviceStyle}>
             <BasicCard className="basic-card--offwhite">
                 <Icon className="icon-basic-webpage-img-txt" />
-                <img src={bambooDivider} style={{ marginBottom: 0}}/>
-                <h4>Custom CMS Development</h4>
-                <p>Do you need a blog or small business website? Why not have something custom that will allow you to easily add new content through a smooth, intuitive interface. I can help you build, maintain, and update your page.</p>
-                <Button>Learn More</Button>
+                <h4 style={{ marginTop: 0}}>Custom CMS Development</h4>
             </BasicCard>
             <BasicCard className="basic-card--offwhite">
                 <Icon className="icon-basic-laptop" />
-                <img src={bambooDivider} style={{ marginBottom: 0}}/>
-                <h4>Web Development</h4>
+                <h4 style={{ marginTop: 0}}>Web Development</h4>
                 <p>Whether you want something built from scratch or you could use an extra set of hands on a current project, I can help you turn your idea into a beautiful, responsive, functional web application.</p>
                 <Button>Learn More</Button>
             </BasicCard>
             <BasicCard className="basic-card--offwhite">
                 <Icon className="icon-basic-smartphone" />
-                <img src={bambooDivider} style={{ marginBottom: 0}}/>
-                <h4>Mobile Development</h4>
+                <h4 style={{ marginTop: 0}}>Mobile Development</h4>
                 <p>Engage your users on the device that is always in their possesion. I can help build you a mobile app or contribute to your exisiting team of app developers</p>
                 <Button>Learn More</Button>
             </BasicCard>
