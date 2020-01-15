10e406484a466c8b3f44421d7415580a9441c696
Sore <soren@sorenbaird.com>
2020-01-15 13:22:54 -0800

    Added Envivent alongside of 360 BC Group?

--- src/sections/mainWork.jsx
+++ src/sections/mainWork.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import SectionTitle from '../components/sectionTitle';
 import StitchRows from '../components/stitchRows';
 import bcgroup from '../images/work/360logo.png';
+import envivent from '../images/work/envivent.svg';
 
 const styles = {
   mainWorkStyle: {
@@ -39,6 +40,12 @@ const styles = {
     width: '100%',
     maxWidth: '300px',
   },
+  enviventLogoStyle: {
+    width: '100%',
+    maxWidth: '400px',
+    display: 'block',
+    margin: '0 auto',
+  },
   textContainerStyle: {
     padding: '0 2rem',
     minWidth: '50%',
@@ -64,7 +71,7 @@ const MainWork = ({ activeSet, angledLineHeight }) => {
         angledLineHeight={angledLineHeight}
         rotate={rotate}
       >
-        <div style={styles.contentWrapperStyle} className="work__main-work">
+        <div style={styles.contentWrapperStyle} className="work__main-work-1">
           <div style={styles.logoContainerStyle}>
             <a
               href="https://www.360bcgroup.com/"
@@ -77,13 +84,39 @@ const MainWork = ({ activeSet, angledLineHeight }) => {
             </a>
           </div>
           <div style={styles.textContainerStyle}>
+            {/* <h3 style={{ textAlign: 'center' }}>Spring 2019 - Present</h3> */}
             <p>
-              I am currently a full-time CMS Developer at <a href="https://www.360bcgroup.com/" target="_blank" rel="noopener noreferrer">360 BC Group Inc.</a>, a parent corporation for three B2B companies: 360Biz, 360Civic, and 360Circuits.
+              I am currently a full-time CMS Developer at <a href="https://www.360bcgroup.com/" target="_blank" rel="noopener noreferrer">360 BC Group</a>, a parent corporation for three B2B companies: 360Biz, 360Civic, and 360Circuits.
             </p>
             <p>
-              I work primarily with 360Civic, building websites for public sector entities
-              (school districts, utiliaties, municipalities) and nonprofit organizations.
+              I work primarily with 360Civic, building large websites with custom functionality for
+              public sector entities (school districts, cities, counties, utilities).
             </p>
+          </div>
+        </div>
+        <div style={styles.contentWrapperStyle} className="work__main-work-2">
+          <div style={styles.textContainerStyle}>
+            {/* <h3 style={{ textAlign: 'center' }}>Winter 2017 - Spring 2019</h3> */}
+            <p>
+              Previously, I worked at Envivent Web Development, working primarily with
+              healthcare ad agencies, acting as their development partner.
+            </p>
+            <p>
+              I built traditional websites, content management systems, interactive tools,
+              data management portals, closed loop marketing solutions, email campaigns,
+              trade show panels, Salesforce.com apps/websites, and more.
+            </p>
+          </div>
+          <div style={styles.logoContainerStyle}>
+            <a
+              href="https://envivent.com"
+              className="work__envivent-link"
+              rel="noopener noreferrer"
+              target="_blank"
+              style={{ width: '100%' }}
+            >
+              <img src={envivent} style={styles.enviventLogoStyle} alt="Envivent Web Development" />
+            </a>
           </div>
         </div>
       </StitchRows>
